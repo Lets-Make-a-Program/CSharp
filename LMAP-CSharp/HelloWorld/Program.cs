4305c83c75d403f483d05992ec1ead7d8de0d335
Noah Wright <noahwright87@gmail.com>
2021-11-06 15:16:03 -0500

    Added C# Hello World project

---
+++ LMAP-CSharp/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("How many loops?  ");
+            int userT;
+            while (!int.TryParse(Console.ReadLine(), out userT))
+            {
+                Console.WriteLine("You must enter a number!  Try again.");
+            }    
+            for (int t = userT; t > 0; t--)
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CSharp
-Project files for the C# programs of Let's Make a Program 
+# Let's Make a Program in C#
+This repo contains the project files for the C# programs of Let's Make a Program.  For the guided tutorials
+that go along with the code in this repo, check out out the C# section of  
+[Let's Make a Program](https://letsmakeaprogram.com/CSharp/).
